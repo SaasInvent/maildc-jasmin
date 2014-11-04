c6686ed988544554b71ac2bede2a37265d948037
WorkingBox <contact@working-box.com>
2014-11-04 12:05:33 +0000

    lib

--- client/lib/maildc-flash.js
+++ client/lib/maildc-flash.js
@@ -1,7 +1,7 @@
 // return functions from flash
 
 function onInit() {
-      flash = $("#flash"); 
+      var flash = $("#flash"); 
       console.log (flash) ;        
       console.log ('onInit');
       flash[0].getIndex("hallo"); 
@@ -11,6 +11,9 @@ function onInit() {
 function onGetIndex(index) {
     console.log ("onGetIndex");
     console.log (index);
+    console.log ($.parseJSON( index ));
+    var myObject = jQuery.parseJSON( index);
+    console.log (myObject.name);
 }
 
 
